95b93030314c7a50505f71567d209c979e4c47c3
V <j92080412@gmail.com>
2024-06-05 17:07:29 +0800

    LaserReminder

--- Snake/Assets/_Snake/Scripts/Laser/LaserController.cs
+++ Snake/Assets/_Snake/Scripts/Laser/LaserController.cs
@@ -16,12 +16,28 @@ public class LaserController : MonoBehaviour
     [SerializeField] private GameObject _startVFX;
     [SerializeField] private GameObject _endVFX;
 
+    [Header("Remind Laser")]
+    [SerializeField] private LineRenderer _remindRenderer;
+    [SerializeField] private GameObject _remindLaser;
+    [SerializeField] private Material _remindMat;
+    [SerializeField] private Color _remindColor = Color.red;
+    private bool isStillRemind = true;
+    private Coroutine _flashCoro;
+    private Coroutine _LaserREmindCoro;
+
     private  List<ParticleSystem> particles = new List<ParticleSystem>();
 
     #region LC
     private void Start() 
     {
         SetupParticleList();
+
+        _remindMat = _remindRenderer.material;
+        if(_LaserREmindCoro != null)
+        {
+            StopCoroutine(_LaserREmindCoro);
+        }
+        StartCoroutine(Coroutine_LaserReminder());
     }
 
     private void Update() 
@@ -43,6 +59,8 @@ public class LaserController : MonoBehaviour
     private void UpdateLaser()
     {
         _lineRenderer.SetPosition(0 , _startPoint.position);
+        _remindRenderer.SetPosition(0 , _startPoint.position);
+
         _startVFX.transform.position = _startPoint.position;   
         Vector2 endPoint = _endPoint.transform.position;
 
@@ -52,11 +70,17 @@ public class LaserController : MonoBehaviour
         if(hitPos)
         {
             _lineRenderer.SetPosition(1 , hitPos.point);
-            Damage(hitPos);
+            _remindRenderer.SetPosition(1 , hitPos.point);
+
+            if(!isStillRemind)
+            {
+                Damage(hitPos);
+            }
         }
         else
         {
             _lineRenderer.SetPosition(1 , endPoint);
+            _remindRenderer.SetPosition(1 , endPoint);
         }
 
         _endVFX.transform.position = _lineRenderer.GetPosition(1);      
@@ -93,4 +117,45 @@ public class LaserController : MonoBehaviour
             }
         }
     }
+
+    private IEnumerator Coroutine_LaserReminder()
+    {
+        _lineRenderer.enabled = false;
+
+        StartFlash(.4f);
+        yield return new WaitForSeconds(.4f);
+        StartFlash(.4f);
+        yield return new WaitForSeconds(.4f);
+        StartFlash(.4f);
+        yield return new WaitForSeconds(.4f);
+        StartFlash(.4f);
+        yield return new WaitForSeconds(.4f);
+
+        _remindLaser.SetActive(false);
+        isStillRemind = false;
+        _lineRenderer.enabled = true;
+    }
+    public void StartFlash(float time)
+    {
+        if(_flashCoro != null)
+        {
+            StopCoroutine(_flashCoro);
+        }
+        StartCoroutine(Coroutine_Flash(time));
+    }
+    private IEnumerator Coroutine_Flash(float time)
+    {
+        _remindMat.SetColor("_Color", _remindColor * 2f);
+
+        float currentFlashAmount = 0f;
+        float elaTime = 0f;
+        while(elaTime < time)
+        {
+            elaTime += Time.deltaTime;
+
+            currentFlashAmount = Mathf.Lerp(1f, .2f, (elaTime / time));
+            _remindMat.SetFloat("_Intensity", currentFlashAmount);
+            yield return null;
+        }
+    }    
 }
