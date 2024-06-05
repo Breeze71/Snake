c10e2ed195a62b8234dd70cd25aba414d7443042
demineer <2361368702@qq.com>
2024-06-05 17:30:54 +0800

    血条UI

--- Snake/Assets/_Snake/Scripts/UI/PlayerHealthUI.cs
+++ Snake/Assets/_Snake/Scripts/UI/PlayerHealthUI.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealthUI : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _healthBlocks;
+    [SerializeField] private List<Sprite> _hurtBlocks;
+    [SerializeField] private List<Sprite> _fullhealth;
     [SerializeField] private SnakeManager _snake;
 
     private void Start() 
@@ -17,12 +20,19 @@ public class PlayerHealthUI : MonoBehaviour
     {
         foreach(GameObject healthBlock in _healthBlocks)
         {
-            healthBlock.SetActive(false);
+            //healthBlock.SetActive(false);
+            //healthBlock.GetComponent<Image>().sprite=_hu
+        }
+
+        for (int i = 0; i < _healthBlocks.Count; i++)
+        {
+            _healthBlocks[i].GetComponent<Image>().sprite = _hurtBlocks[i];
         }
 
         for(int i = 0; i < currentHealth; i++)
         {
-            _healthBlocks[i].SetActive(true);
+            //_healthBlocks[i].SetActive(true);
+            _healthBlocks[i].GetComponent<Image>().sprite = _fullhealth[i];
         }
     }
 }
